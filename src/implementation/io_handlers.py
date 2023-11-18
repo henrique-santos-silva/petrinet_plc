bb518797e9bccee7f0ad9fbc86a8f69f46def5b7
Henrique dos Santos Silva <hss.henrique.silva@gmail.com>
2023-11-18 19:09:13 -0300

    create IOHandlersWrapper class

--- src/implementation/io_handlers.py
+++ src/implementation/io_handlers.py
@@ -9,8 +9,38 @@ from src.abstract.abstract_io_handler import AbstractIOHandler
 from src.abstract.abstract_bool_parser import AbstractBoolParser
 from src.abstract.abstract_petri_net_subcomponents import AbstractPetriNetPlace
 
+class IOHandlersWrapper(AbstractIOHandler):
+    def __init__(self,io_handler_emulator:AbstractIOHandler,io_handler_physical:AbstractIOHandler) -> None:
+        self._io_handler_emulator = io_handler_emulator
+        self._io_handler_physical = io_handler_physical
+        self._io_handler = self._io_handler_physical
+    
+    def select_io_handler_emulator(self):
+        print("select_io_handler_emulator")
+        self._io_handler = self._io_handler_emulator
+
+    def select_io_handler_physical(self):
+        print("select_io_handler_physical")
+        self._io_handler = self._io_handler_emulator
+
+    @property
+    def has_been_updated(self) -> bool:
+        return self._io_handler.has_been_updated
+    
+    def set_marking_to_output_expressions(self, marking_to_output_expressions: dict[str, str], BoolParserClass: type[AbstractBoolParser]) -> None:
+        return self._io_handler.set_marking_to_output_expressions(marking_to_output_expressions, BoolParserClass)
+    
+    def update_outputs(self, places: dict[str, AbstractPetriNetPlace]) -> dict[str, dict[str, bool]]:
+        return self._io_handler.update_outputs(places)
+    
+    def clear(self):
+        self._io_handler_emulator.clear()
+        self._io_handler_physical.clear()
+    
+    def get_all(self) -> dict[str, dict[str, bool]]:
+        return self._io_handler.get_all()
+
 class IOWebMocker(AbstractIOHandler):
-    # _template_dir = 
     def __init__(self,
                  digital_inputs:dict[str,bool]|None = None,
                  digital_outputs:dict[str,bool]|None = None,
@@ -68,7 +98,6 @@ class IOWebMocker(AbstractIOHandler):
             @app.route("/")
             def index():
                 return app.send_static_file("index.html")
-                return send_from_directory('IOWebMocker_static', 'index.html')
             
 
             @socketio.on("input_update")
