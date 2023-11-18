de31a0d9dab21e812eee11e272d1b24ddbddf4ec
Henrique dos Santos Silva <hss.henrique.silva@gmail.com>
2023-11-18 08:33:05 -0300

    refactor validation and dom generation

--- src/implementation/webserver_ihm_static/iopt_config_dom_generation.js
+++ src/implementation/webserver_ihm_static/iopt_config_dom_generation.js
@@ -91,143 +91,18 @@ function petrinet_xml2json(file) {
 
   
 
-function generate_IOPT_config_div(IOPT_dictionary,loaded_from_json){
-    // -------------- Input to Transitons-------------------------------------------------------------
-    const transition_signal_enabling_condition_Container = document.getElementById('transition_signal_enabling_condition_container');
 
-    transition_signal_enabling_condition_Container.textContent=""
-    let transitions = [...IOPT_dictionary["instantaneous_transitions"],...IOPT_dictionary["timed_transitions"]]
-    transitions.forEach((transition,index)=>{
 
-      const transitionName = transition["id"]
-      const outerDiv = document.createElement('div');
-      outerDiv.classList.add('row', 'my-1');
+
+function generate_IOPT_config_div(
+  IOPT_dictionary, //dict
+  loaded_from_json, //bool
+  inputs_name_list, //list[str]
+  outputs_name_list, //list[str]
+){
+    generate_transition_signal_enabling_condition_container(IOPT_dictionary, loaded_from_json, inputs_name_list)
+    generate_output_activation_condition_container(IOPT_dictionary, loaded_from_json, outputs_name_list )
     
-      // Cria a coluna para o label
-      const labelCol = document.createElement('div');
-      labelCol.classList.add('col-md-3', 'd-flex', 'align-items-center');
-    
-      // Cria a etiqueta <label>
-      const label = document.createElement('label');
-      label.classList.add('form-label');
-      label.setAttribute('for', `transition_signal_enabling_condition-${transitionName}`);
-      label.textContent = transitionName;
-    
-      // Anexa o label à sua coluna
-      labelCol.appendChild(label);
-    
-      // Cria a coluna para o input
-      const inputCol = document.createElement('div');
-      inputCol.classList.add('col-md-9');
-    
-      // Cria o input
-      const input = document.createElement('input');
-      input.setAttribute('type', 'text');
-      input.classList.add('form-control'); // Bootstrap class for inputs
-      input.setAttribute('name', `transition_signal_enabling_condition-${transitionName}`);
-      input.setAttribute('id', `transition_signal_enabling_condition-${transitionName}`); 
-      input.setAttribute('autocomplete', `off`); 
-      
-
-
-      input.addEventListener("change", function(){
-        extra_tokens = ["i0","i1","i2","i3","i4","i5","i6","i7",]
-        let valid_expression_bool;
-        let new_string; 
-        [valid_expression_bool,new_string] = is_valid_expression($(this).val(),extra_tokens)
-        if (valid_expression_bool && new_string != ""){
-            $(this).attr('class','form-control transition_signal_enabling_condition_textfield')
-            $(this).val(new_string)
-        } else {
-            $(this).attr('class','form-control transition_signal_enabling_condition_textfield border border-danger')
-        }
-      });
-
-
-      if (loaded_from_json){
-        input.value = transition["signal_enabling_expression"]
-        const event = new Event('change');
-        input.dispatchEvent(event);
-      }else{
-        input.value = "true"
-      }
-    
-      // Anexa o input à sua coluna
-      inputCol.appendChild(input);
-    
-      // Anexa as colunas à div externa
-      outerDiv.appendChild(labelCol);
-      outerDiv.appendChild(inputCol);
-    
-      // Anexa a div externa à div principal
-      transition_signal_enabling_condition_Container.appendChild(outerDiv);
-    });
-    
-
-    // output_activation_condition_container
-    // Seleciona o container
-    const output_activation_condition_Container = document.getElementById('output_activation_condition_container');
-    output_activation_condition_Container.textContent = "";
-
-    for (let i = 0; i < 8; i++) {
-      const outputName = `o${i}`;
-      const outerDiv = document.createElement('div');
-      outerDiv.classList.add('row', 'my-1'); // Usando 'row' em vez de 'd-flex'
-
-      // Cria a coluna para o label
-      const labelCol = document.createElement('div');
-      labelCol.classList.add('col-md-3', 'd-flex', 'align-items-center'); // Definindo o tamanho da coluna e alinhamento
-
-      // Cria a etiqueta <label>
-      const label = document.createElement('label');
-      label.classList.add('form-label');
-      label.setAttribute('for', `output_activation_condition-${outputName}`);
-      label.textContent = outputName;
-      labelCol.appendChild(label); // Anexa o label à sua coluna
-
-      // Cria a coluna para o input
-      const inputCol = document.createElement('div');
-      inputCol.classList.add('col-md-9'); // Definindo o tamanho da coluna
-
-      // Cria o input
-      const input = document.createElement('input');
-      input.setAttribute('type', 'text');
-      input.classList.add('form-control'); // Usando 'form-control' para estilização do Bootstrap
-      input.setAttribute('name', `output_activation_condition-${outputName}`);
-      input.setAttribute('id', `output_activation_condition-${outputName}`);
-      // ...restante do código do input...
-
-      input.addEventListener("change", function(){
-          
-        let extra_tokens = IOPT_dictionary["places"].map(function(place){return place["id"]})
-        let valid_expression_bool;
-        let new_string; 
-        [valid_expression_bool,new_string] = is_valid_expression($(this).val(),extra_tokens)
-        if (valid_expression_bool && new_string != ""){
-          $(this).attr('class','form-control output_activation_condition_textfield')
-          $(this).val(new_string)
-        } else {
-          $(this).attr('class','form-control output_activation_condition_textfield border border-danger')
-        }
-      });
-      
-      if (loaded_from_json){
-        input.value=IOPT_dictionary["marking_to_output_expressions"][outputName]
-        const event = new Event('change');
-        input.dispatchEvent(event);
-      }else{
-        input.value = "false"
-      }      
-      inputCol.appendChild(input);
-
-      // Anexa as colunas à div externa
-      outerDiv.appendChild(labelCol);
-      outerDiv.appendChild(inputCol);
-
-      // Anexa a div externa à div principal
-      output_activation_condition_Container.appendChild(outerDiv);
-    }
-
     //--------------------------------
     const timed_transition_timer_Element = document.getElementById('timed_transition_timer_container');
     timed_transition_timer_Element.textContent = "";
@@ -261,6 +136,7 @@ function generate_IOPT_config_div(IOPT_dictionary,loaded_from_json){
       input.setAttribute('type', 'number');
       input.setAttribute('step', '0.01');
       input.classList.add('form-control'); // Classe do Bootstrap para estilização dos inputs
+      input.classList.add('timed_transition_timer_textfield');
       input.setAttribute('name', `timed_transition_timer-${transitionName}`);
       input.setAttribute('id', `timed_transition_timer-${transitionName}`);
       input.setAttribute('required', true);
@@ -280,4 +156,136 @@ function generate_IOPT_config_div(IOPT_dictionary,loaded_from_json){
 
 
     $('#IOPT_config').show()
+}
+
+
+function generate_transition_signal_enabling_condition_container(
+  IOPT_dictionary, //dict
+  loaded_from_json, //bool
+  inputs_name_list //list[str]
+){
+  // -------------- Input to Transitons-------------------------------------------------------------
+  const transition_signal_enabling_condition_container = document.getElementById('transition_signal_enabling_condition_container');
+
+  transition_signal_enabling_condition_container.textContent="";
+  const transitions = [...IOPT_dictionary["instantaneous_transitions"],...IOPT_dictionary["timed_transitions"]];
+  
+  for (const transition of transitions){
+    const transitionName = transition.id;
+
+    const outerDiv = document.createElement('div');
+    outerDiv.classList.add('row', 'my-1');
+
+      const labelCol = document.createElement('div');
+      labelCol.classList.add('col-md-3', 'd-flex', 'align-items-center');
+
+        const label = document.createElement('label');
+        label.classList.add('form-label');
+        label.setAttribute('for', `transition_signal_enabling_condition-${transitionName}`);
+        label.textContent = transitionName;
+      
+        labelCol.appendChild(label);
+      outerDiv.appendChild(labelCol);
+    
+
+      const inputCol = document.createElement('div');
+      inputCol.classList.add('col-md-9');
+        
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.classList.add('form-control');
+        input.classList.add('transition_signal_enabling_condition_textfield');
+
+        input.setAttribute('name', `transition_signal_enabling_condition-${transitionName}`);
+        input.setAttribute('id', `transition_signal_enabling_condition-${transitionName}`); 
+        input.setAttribute('autocomplete', `off`); 
+
+        input.addEventListener("change", function(){
+          const extra_tokens = inputs_name_list
+          let valid_expression_bool;
+          let new_string; 
+          [valid_expression_bool,new_string] = is_valid_expression($(this).val(),extra_tokens);
+          if (valid_expression_bool && new_string != ""){
+              $(this).attr('class','form-control transition_signal_enabling_condition_textfield');
+              $(this).val(new_string);
+          } else {
+              $(this).attr('class','form-control transition_signal_enabling_condition_textfield border border-danger');
+          }
+        });
+
+        if (loaded_from_json){
+          input.value = transition["signal_enabling_expression"];
+          const event = new Event('change');
+          input.dispatchEvent(event);
+        }else{
+          input.value = "true";
+        }
+
+        inputCol.appendChild(input);
+      outerDiv.appendChild(inputCol);
+      transition_signal_enabling_condition_container.appendChild(outerDiv);
+  }
+}
+
+function generate_output_activation_condition_container(
+  IOPT_dictionary, //dict
+  loaded_from_json, //bool
+  outputs_name_list //list[str]
+){
+  const output_activation_condition_container = document.getElementById('output_activation_condition_container');
+  output_activation_condition_container.textContent = "";
+
+  for (const output_name of outputs_name_list){
+    const outerDiv = document.createElement('div');
+    outerDiv.classList.add('row', 'my-1');
+
+      // Cria a coluna para o label
+      const labelCol = document.createElement('div');
+      labelCol.classList.add('col-md-3', 'd-flex', 'align-items-center'); // Definindo o tamanho da coluna e alinhamento
+
+        // Cria a etiqueta <label>
+        const label = document.createElement('label');
+        label.classList.add('form-label');
+        label.setAttribute('for', `output_activation_condition-${output_name}`);
+        label.textContent = output_name;
+        labelCol.appendChild(label); // Anexa o label à sua coluna
+      outerDiv.appendChild(labelCol);
+
+      // Cria a coluna para o input
+      const inputCol = document.createElement('div');
+      inputCol.classList.add('col-md-9'); // Definindo o tamanho da coluna
+
+        // Cria o input
+        const input = document.createElement('input');
+        input.setAttribute('type', 'text');
+        input.classList.add('form-control'); // Usando 'form-control' para estilização do Bootstrap
+        input.classList.add('output_activation_condition_textfield'); // Usando 'form-control' para estilização do Bootstrap
+        input.setAttribute('name', `output_activation_condition-${output_name}`);
+        input.setAttribute('id', `output_activation_condition-${output_name}`);
+        input.addEventListener("change", function(){
+        
+          let extra_tokens = IOPT_dictionary["places"].map(function(place){return place["id"]})
+          let valid_expression_bool;
+          let new_string; 
+          [valid_expression_bool,new_string] = is_valid_expression($(this).val(),extra_tokens)
+          if (valid_expression_bool && new_string != ""){
+            $(this).attr('class','form-control output_activation_condition_textfield')
+            $(this).val(new_string)
+          } else {
+            $(this).attr('class','form-control output_activation_condition_textfield border border-danger')
+          }
+        });
+        
+        if (loaded_from_json){
+          input.value=IOPT_dictionary["marking_to_output_expressions"][output_name]
+          const event = new Event('change');
+          input.dispatchEvent(event);
+        }else{
+          input.value = "false"
+        }
+        inputCol.appendChild(input);
+      outerDiv.appendChild(inputCol);
+      output_activation_condition_container.appendChild(outerDiv);
+  }
+
 }
