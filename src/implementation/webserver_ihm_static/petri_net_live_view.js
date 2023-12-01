39d6c987f840d08dc5082439634880a888ac3f12
Henrique dos Santos Silva <hss.henrique.silva@gmail.com>
2023-12-01 12:22:16 -0300

    create petri_net subcomponents classes

---
+++ src/implementation/webserver_ihm_static/petri_net_live_view.js
@@ -0,0 +1,45 @@
+// Class to represent a position with x and y coordinates
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+// Class for labels with text and a position
+class Label {
+    constructor(text, position) {
+        this.text = text;
+        this.position = position; // Position should be an instance of Position
+    }
+    
+}
+
+// Class for transitions in a Petri net
+class Transition {
+    constructor(x, y, isEnabled, label) {
+        this.position = new Position(x, y);
+        this.isEnabled = isEnabled; // Indicates if the transition is enabled
+        this.label = label;
+    }
+}
+
+// Class for places in a Petri net
+class Place {
+    constructor(x, y, marking, label) {
+        this.position = new Position(x, y);
+        this.marking = marking; // A number indicating the number of tokens
+        this.label = label;
+    }
+}
+
+// Class for arcs in a Petri net
+class Arc {
+    constructor(weight, isInhibitor, label, path) {
+        this.weight = weight; // Weight of the arc
+        this.isInhibitor = isInhibitor; // Boolean indicating if the arc is an inhibitor
+        this.label = label;
+        this.path = path; // Path should be a list of Position instances
+    }
+}
+
